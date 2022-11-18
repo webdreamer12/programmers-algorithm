9c714aaf9b06697d65a15db59b4cb98ae8555d67
Peter Kang <113353436+webdreamer12@users.noreply.github.com>
2022-11-18 11:46:27 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,12 @@
+let solution = (price) => {
+  let discountedPrice = price;
+  if (price >= 500000) {
+    return parseInt(price * 0.8);
+  } else if (price >= 300000) {
+    return parseInt(price * 0.9);
+  } else if (price >= 100000) {
+    return parseInt(price * 0.95);
+  } else {
+    return discountedPrice;
+  }
+};
