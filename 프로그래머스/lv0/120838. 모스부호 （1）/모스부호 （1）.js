f80346980dba2cb2523319eb5eb0e5ef455aa451
Peter Kang <113353436+webdreamer12@users.noreply.github.com>
2022-11-24 15:40:21 +0900

    [level 0] Title: 모스부호 (1), Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,36 @@
+let solution = (letter) => {
+  const morse = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+  };
+
+  let answer = letter.split(" ");
+  for (let order = 0; order < answer.length; order++) {
+    answer[order] = morse[answer[order]];
+  }
+  return answer.join("");
+};
